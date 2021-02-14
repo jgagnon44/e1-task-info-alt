39984bd1ea1fe1bd06aee9eac2e96d2d19076178
jgagnon44 <56738211+jgagnon44@users.noreply.github.com>
2021-02-14 11:17:07 -0500

    Update TaskHierarchiesView.java

--- task-viewer/src/main/java/com/fossfloors/e1tasks/ui/TaskHierarchiesView.java
+++ task-viewer/src/main/java/com/fossfloors/e1tasks/ui/TaskHierarchiesView.java
@@ -3,27 +3,33 @@ package com.fossfloors.e1tasks.ui;
 import javax.annotation.PostConstruct;
 
 import com.fossfloors.e1tasks.backend.service.TaskMasterService;
+import com.fossfloors.e1tasks.backend.service.TaskRelationshipService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout.Orientation;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("tree")
 @CssImport("./styles/shared-styles.css")
 public class TaskHierarchiesView extends VerticalLayout {
 
-  private static final long serialVersionUID = 1L;
+  private static final long       serialVersionUID = 1L;
 
-  private TopLevelTaskView  topLevelView;
-  private TaskTreeView      taskTreeView;
-  private TaskDetailView    taskDetailView;
+  private TopLevelTaskView        topLevelView;
+  private TaskTreeView            taskTreeView;
+  private TaskDetailView          taskDetailView;
 
-  private TaskMasterService taskService;
+  private TaskMasterService       taskService;
+  private TaskRelationshipService taskRelationService;
 
-  public TaskHierarchiesView(TaskMasterService taskService) {
+  public TaskHierarchiesView(TaskMasterService taskService,
+      TaskRelationshipService taskRelationService) {
     this.taskService = taskService;
+    this.taskRelationService = taskRelationService;
 
     setSizeFull();
     addClassName("task-hierarchies-view");
@@ -37,38 +43,38 @@ public class TaskHierarchiesView extends VerticalLayout {
   }
 
   private void configureView() {
-    add(/* configInfoPane(), */ configGridsLayout());
+    add(configInfoPane(), configGridsLayout());
 
     topLevelView.addListener(TopLevelTaskView.TaskSelectionEvent.class, taskTreeView::loadTask);
     taskTreeView.addListener(TaskTreeView.TaskSelectionEvent.class, taskDetailView::loadTask);
   }
 
-  // private Component configInfoPane() {
-  // HorizontalLayout layout = new HorizontalLayout();
-  //
-  // TextField pt = new TextField("# parent tasks");
-  // pt.setReadOnly(true);
-  // pt.setValue(String.valueOf(taskRelationService.getParentTaskIDs().size()));
-  //
-  // TextField dpt = new TextField("# distinct parent tasks");
-  // dpt.setReadOnly(true);
-  // dpt.setValue(String.valueOf(taskRelationService.getDistinctParentTaskIDs().size()));
-  //
-  // TextField ct = new TextField("# child tasks");
-  // ct.setReadOnly(true);
-  // ct.setValue(String.valueOf(taskRelationService.getChildTaskIDs().size()));
-  //
-  // TextField dct = new TextField("# distinct child tasks");
-  // dct.setReadOnly(true);
-  // dct.setValue(String.valueOf(taskRelationService.getDistinctChildTaskIDs().size()));
-  //
-  // TextField tlt = new TextField("# top-level tasks");
-  // tlt.setReadOnly(true);
-  // tlt.setValue(String.valueOf(taskRelationService.getTopLevelParentTaskIDs().size()));
-  //
-  // layout.add(pt, dpt, ct, dct, tlt);
-  // return layout;
-  // }
+  private Component configInfoPane() {
+    HorizontalLayout layout = new HorizontalLayout();
+
+    TextField pt = new TextField("# parent tasks");
+    pt.setReadOnly(true);
+    pt.setValue(String.valueOf(taskRelationService.getParentTaskIDs().size()));
+
+    TextField dpt = new TextField("# distinct parent tasks");
+    dpt.setReadOnly(true);
+    dpt.setValue(String.valueOf(taskRelationService.getDistinctParentTaskIDs().size()));
+
+    TextField ct = new TextField("# child tasks");
+    ct.setReadOnly(true);
+    ct.setValue(String.valueOf(taskRelationService.getChildTaskIDs().size()));
+
+    TextField dct = new TextField("# distinct child tasks");
+    dct.setReadOnly(true);
+    dct.setValue(String.valueOf(taskRelationService.getDistinctChildTaskIDs().size()));
+
+    TextField tlt = new TextField("# top-level tasks");
+    tlt.setReadOnly(true);
+    tlt.setValue(String.valueOf(taskRelationService.getTopLevelParentTaskIDs().size()));
+
+    layout.add(pt, dpt, ct, dct, tlt);
+    return layout;
+  }
 
   private Component configGridsLayout() {
     SplitLayout layout = new SplitLayout();
