f286573c743c1ace77bd8759909440a022462986
jgagnon44 <56738211+jgagnon44@users.noreply.github.com>
2021-07-22 14:45:09 -0400

    Add getMasterMenuMap().

--- backend/src/main/java/com/fossfloors/e1tasks/backend/service/TaskService.java
+++ backend/src/main/java/com/fossfloors/e1tasks/backend/service/TaskService.java
@@ -1,5 +1,8 @@
 package com.fossfloors.e1tasks.backend.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.transaction.Transactional;
@@ -31,6 +34,17 @@ public class TaskService {
 
   public void saveAll(Set<Task> taskSet) {
     taskRepo.saveAll(taskSet);
+  }
+
+  public Map<String, Set<Task>> getMasterMenuMap(List<String> taskViews) {
+    Map<String, Set<Task>> result = new HashMap<>();
+
+    taskViews.forEach(taskView -> {
+      List<Task> viewTasks = taskRepo.findAllForTaskView(taskView);
+      result.put(taskView, Set.copyOf(viewTasks));
+    });
+
+    return result;
   }
 
 }
